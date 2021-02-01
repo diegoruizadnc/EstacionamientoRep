f9c5c92516c4e7548c9c78b0b0d9ae1292ba5ad7
DiegoRuiz <diegoruizdx@gmail.com>
2021-02-01 14:25:33 -0500

    quito unos imports

--- src/test/java/com/ceiba/reservasbarultimate/ReservasbarultimateApplicationTests.java
+++ src/test/java/com/ceiba/reservasbarultimate/ReservasbarultimateApplicationTests.java
@@ -1,15 +1,8 @@
 package com.ceiba.reservasbarultimate;
-
-import static org.junit.Assert.assertTrue;
-
-import java.util.Date;
-
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ceiba.reservasbarultimate.reserva.dominio.modelo.entidad.excepciones.ValidadorCampos;
+
 
 @SpringBootTest
 class ReservasbarultimateApplicationTests {
