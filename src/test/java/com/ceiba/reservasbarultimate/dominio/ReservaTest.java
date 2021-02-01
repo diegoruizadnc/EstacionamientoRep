5bb3432216550948d9098f1dcaefa69d2f2c9c2e
DiegoRuiz <diegoruizdx@gmail.com>
2021-02-01 17:34:11 -0500

    verificar covertura pruebas

--- src/test/java/com/ceiba/reservasbarultimate/dominio/ReservaTest.java
+++ src/test/java/com/ceiba/reservasbarultimate/dominio/ReservaTest.java
@@ -15,13 +15,67 @@ public class ReservaTest {
 	}
 	
 	@Test
+	public void crearReservaNumeroMesaNulo() throws Exception{
+		
+		//arrange
+		Date fecha = new Date();
+		//act
+		try {
+		@SuppressWarnings("unused")
+		Reserva reserva = new Reserva(null,10302l,fecha,5,180000l);
+		//assert
+		}catch (Exception e) {
+			assertEquals("error, El numero de mesa debe ser un valor entre 1 y 9", e.getMessage() );
+		}
+		
+		
+	}
+	
+	@Test
+	public void crearReservaNumeroMesaMayorNueve() throws Exception{
+		
+		//arrange
+		Date fecha = new Date();
+		//act
+		try {
+		@SuppressWarnings("unused")
+		Reserva reserva = new Reserva(10,10302l,fecha,5,180000l);
+		//assert
+		}catch (Exception e) {
+			assertEquals("error, El numero de mesa debe ser un valor entre 1 y 9", e.getMessage() );
+		}
+		
+		
+	}
+	
+	@Test
+	public void crearReservaNumeroMesaNegativo() throws Exception{
+		
+		//arrange
+		Date fecha = new Date();
+		//act
+		try {
+		@SuppressWarnings("unused")
+		Reserva reserva = new Reserva(-6,10302l,fecha,5,180000l);
+		//assert
+		}catch (Exception e) {
+			assertEquals("error, El numero de mesa debe ser un valor entre 1 y 9", e.getMessage() );
+		}
+		
+		
+	}
+	
+	@Test
 	public void crearReservaDiaNoValido() throws Exception{
 		
+		//arrange
 		Date fecha = new Date();
+		//act
 		try {
 		Reserva reserva = new Reserva(1,10302l,fecha,5,180000l);
+		//assert
 		}catch (Exception e) {
-			assertEquals(e.getMessage(), "error, Solo puede hacer las reservas de miercoles a sabado");
+			assertEquals("error, Solo puede hacer las reservas de miercoles a sabado",e.getMessage() );
 		}
 		
 		
@@ -29,11 +83,14 @@ public class ReservaTest {
 	
 	@Test
 	public void crearReservaFechaNulo() throws Exception{
-		
+		//arrange
+		Date fecha = null;
+		//act
 		try {
 		Reserva reserva = new Reserva(1,10302l,null,5,180000l);
+		//assert
 		}catch (Exception e) {
-			assertEquals(e.getMessage(), "error, Solo puede hacer las reservas de miercoles a sabado");
+			assertEquals("error, Solo puede hacer las reservas de miercoles a sabado",e.getMessage() );
 		}
 		
 		
