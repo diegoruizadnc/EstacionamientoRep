38ea0cc09cd43e9b521c6b319344a9ad8a4d7e5b
DiegoRuiz <diegoruizdx@gmail.com>
2021-02-02 12:43:12 -0500

    repossitorio reserva persistente test

--- src/test/java/com/ceiba/reservasbarultimate/infraestructura/RepositorioReservaPersistenteTest.java
+++ src/test/java/com/ceiba/reservasbarultimate/infraestructura/RepositorioReservaPersistenteTest.java
@@ -1,4 +1,4 @@
-package com.ceiba.reservasbarultimate.aplicacion.manejador;
+package com.ceiba.reservasbarultimate.infraestructura;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,37 +14,36 @@ import com.ceiba.reservasbarultimate.aplicacion.ComandoReservaTestBuild;
 import com.ceiba.reservasbarultimate.dominio.ReservaDtoTestDataBuild;
 import com.ceiba.reservasbarultimate.dominio.ReservaTestDataBuild;
 import com.ceiba.reservasbarultimate.reserva.aplicacion.comando.ComandoReserva;
-import com.ceiba.reservasbarultimate.reserva.aplicacion.comando.manejador.ManejadorCrearReserva;
 import com.ceiba.reservasbarultimate.reserva.dominio.modelo.dto.ReservaDto;
 import com.ceiba.reservasbarultimate.reserva.dominio.modelo.entidad.Reserva;
+import com.ceiba.reservasbarultimate.reserva.infraestructura.adaptador.repositorio.RepositorioReservaPersistente;
 
 @SpringBootTest
- class ManejadorCrearReservaTest {
-
+public class RepositorioReservaPersistenteTest {
+	
 	@Autowired
-	ManejadorCrearReserva manejadorCrearReserva;
-
-	public ManejadorCrearReservaTest() {
-
+	RepositorioReservaPersistente repositorioReservaPersistente;
+	
+	public RepositorioReservaPersistenteTest(){
+			
 	}
-
+	
 	@Test
-	void ejecutarManejadorCrearReservaTest() {
-
+	void ejecutarCrearReservaTest() {
+		
 		// arrange
 		LocalDate localDate = LocalDate.of(2021, 01, 29);
 		Date fecha = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		ComandoReserva comandoReserva = new ComandoReservaTestBuild().conFechaReserva(fecha).build();
-		@SuppressWarnings("unused")
+	
 		Reserva reserva = new ReservaTestDataBuild().conFechaReserva(fecha).build();
 		ReservaDto reservaDto = new ReservaDtoTestDataBuild().conFechaReserva(fecha).build();
 
 		// act
-		ReservaDto resultadoReservaDto = manejadorCrearReserva.ejecutar(comandoReserva);
+		ReservaDto resultadoReservaDto = repositorioReservaPersistente.crear(reserva);
 
 		// assert
 		assertEquals(reservaDto.getFechaReserva(), resultadoReservaDto.getFechaReserva());
-
+		
 	}
 
 }
